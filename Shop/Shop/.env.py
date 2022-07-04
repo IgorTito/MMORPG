44f6e75d93d7927f4594a78657d5e3a82e052f68
IgorTito <igortito@inbox.ru>
2022-07-04 18:41:25 +0300

    create project

---
+++ Shop/Shop/.env.py
@@ -0,0 +1,6 @@
+#django pass
+SECRET_KEY = 'django-insecure-u+-w1ei*!**0*kx)-cm0d)5lieo)6v-xewp(c1pf1#shx$_^hz'
+
+# smtp password
+EMAIL_HOST_USER = 'hayabusaigor@yandex.ru'
+EMAIL_HOST_PASSWORD = 'Bujhmnbnfhtyrj15'
